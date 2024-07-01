146065ee0235da063b0862948109d6eb9bf41fc1
priti bishnoi <priti12bishnoi@gmail.com>
2024-07-01 11:23:47 +0530

    added some file

--- shopproject/urls.py
+++ shopproject/urls.py
@@ -1,11 +1,19 @@
 
 from django.contrib import admin
 from django.urls import path,include
+
+from django.conf import settings
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from . import settings
 
 from django.conf.urls.static import static
 
 urlpatterns = [
+     path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
